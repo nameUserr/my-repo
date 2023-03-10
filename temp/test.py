9fa0dd5cc10e7bf5bab7855625f9235c3e1d24b4
nameUserr <jackwu055@gmail.com>
2023-03-10 15:38:04 +0800

    ignore temp/test.py

--- temp/test.py
+++ temp/test.py
@@ -1,7 +1,5 @@
-import pandas as pd
-from pandas import Series
-l = [1,2,3,4,5]
-print(l == 1)
-s = Series(l)
-print(s == 1)
-print(pd.cut(s, bins=[0, 2, 4, 6]))
+import re
+
+str = '15DF1G5E1SD5F1E'
+print(re.sub(r'[0-9]*', '*', str))
+print(str.replace(r'[0-9]*', '*'))
